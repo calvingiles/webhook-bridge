26d17edc749800d752d2e4c6ba7a3863e0175323
Calvin Giles <calvin.giles@gmail.com>
2014-09-19 23:49:26 +0100

    Updated translator to return idea string.

--- translator.py
+++ translator.py
@@ -1,3 +1,11 @@
 def translate_payload(request_dict):
-    payload = {'text': str(request_dict)}
+    summary = payload_dict.get('summary')
+    title = payload_dict.get('title')
+    web_url = payload_dict.get('web_url')
+    display_name = payload_dict.get('display_name')
+    text = '<{web_url}|{display_name}> - {title}'
+    text = text.format(title=title, web_url=web_url, display_name=display_name)
+    if len(summary) > 0:
+        text = '{}\n{}'.format(text, summary)
+    payload = {'text': text}
     return payload
