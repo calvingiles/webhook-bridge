bc3151035cc7b6f82227b438812c457e013178d0
Calvin Giles <calvin.giles@gmail.com>
2014-09-20 00:10:03 +0100

    Updated translator to swap out <br>, <strong> and <em> tags for Slack equivs.

--- translator.py
+++ translator.py
@@ -6,6 +6,12 @@ def translate_payload(payload_dict):
     text = '<{web_url}|{display_name}> - {title}'
     text = text.format(title=title, web_url=web_url, display_name=display_name)
     if len(summary) > 0:
+        summary = summary.replace('<br />', '\n')
+        summary = summary.replace('<strong>', '*')
+        summary = summary.replace('</strong>', '*')
+        summary = summary.replace('<em>', '_')
+        summary = summary.replace('</em>', '_')
+
         text = '{}\n{}'.format(text, summary)
     payload = {'text': text}
     return payload
